c128268eb47e2eb4153125b06a4f556a1c8c9a25
PabloSantos-CG <comercial-pablo@outlook.com>
2025-09-03 19:38:10 -0300

    feat: add bind service and interface

--- back-end/app/Providers/AppServiceProvider.php
+++ back-end/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Application\Services\Contracts\UserServiceInterface;
+use App\Application\Services\UserService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            UserServiceInterface::class,
+            UserService::class
+        );
     }
 
     /**
