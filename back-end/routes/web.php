ef096a8c177d6cfaf9bd605f1e933fc8f9035cc7
PabloSantos-CG <comercial-pablo@outlook.com>
2025-09-05 12:15:07 -0300

    feat: cria rotas separadas para admin e usuário comum

--- back-end/routes/web.php
+++ back-end/routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ExpenseController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\CheckAdmin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,10 +16,23 @@ Route::post('/user/logout', [AuthController::class, 'logOut']);
 
 Route::post('/user', [UserController::class, 'store']);
 
+Route::middleware(['auth', CheckAdmin::class])
+    ->controller(AdminController::class)
+    ->group(function () {
+        Route::get('/admin/profile', 'showProfile');
+        Route::put('/admin/profile', 'updateProfile');
+        Route::delete('/admin/profile', 'destroyAccount');
+
+        Route::get('/admin/users', 'index');
+        Route::get('/admin/user/{id}', 'showUser');
+        Route::put('/admin/user/{id}/make-admin', 'makeAdmin');
+        Route::delete('/admin/user/{id}', 'destroyUser');
+    });
+
 Route::middleware('auth')
     ->controller(UserController::class)
     ->group(function () {
-        Route::get('/users', 'index');
+        // adicionar política para verificar se ta pegando informação dele mesmo
         Route::get('/user/{id}', 'show');
         Route::put('/user/{id}', 'update');
         Route::delete('/user/{id}', 'destroy');
