a1461ba816e144153d4586fe39ec3e038a361e21
PabloSantos-CG <comercial-pablo@outlook.com>
2025-09-05 11:27:33 -0300

    feat:  cria usuários falsos

--- back-end/database/seeders/DatabaseSeeder.php
+++ back-end/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'is_admin' => true,
+            'password' => Hash::make('Admin@123'),
         ]);
+
+        User::factory(10)->create();
     }
 }
