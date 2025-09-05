f90db3c4ae6212291f84582975bccd2e7879a0c1
PabloSantos-CG <comercial-pablo@outlook.com>
2025-09-05 17:31:22 -0300

    refactor: muda os comentários

---
+++ back-end/app/Http/Controllers/AdminController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    protected User $loggedUser;
+
+    public function __construct()
+    {
+        $this->loggedUser = Auth::user();
+    }
+
+    // MÉTODOS APLICADOS AO PRÓPRIO USUÁRIO
+    /**
+     * Mostra informações do perfil do admin.
+     */
+    public function showProfile()
+    {
+        //
+    }
+
+    /**
+     * Atualiza informações do perfil do admin.
+     */
+    public function updateProfile()
+    {
+        //
+    }
+
+    /**
+     * Softdelete na conta do admin logado.
+     */
+    public function destroyAccount()
+    {
+        //
+    }
+
+    // MÉTODOS APLICADOS EM OUTROS USUÁRIOS
+    /**
+     * Mostra todos os usuários.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Mostra informações de um usuário específico.
+     */
+    public function showUser(User $user)
+    {
+        //
+    }
+
+    /**
+     * Marca o atributo is_admin como TRUE, tornando o usuário um admin.
+     */
+    public function makeAdmin(User $user)
+    {
+        //
+    }
+
+    /**
+     * Softdelete para um usuário específico.
+     */
+    public function destroyUser(User $user)
+    {
+        //
+    }
+}
