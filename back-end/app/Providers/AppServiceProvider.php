a54cf29875ba5b00c9f8ca34f8c9881986f19935
PabloSantos-CG <comercial-pablo@outlook.com>
2025-09-08 11:51:14 -0300

    feat: vinculação das interfaces com os seus respectivos services

--- back-end/app/Providers/AppServiceProvider.php
+++ back-end/app/Providers/AppServiceProvider.php
@@ -2,8 +2,17 @@
 
 namespace App\Providers;
 
-use App\Application\Services\Contracts\UserServiceInterface;
-use App\Application\Services\UserService;
+use App\Application\Admin\Contracts\AdminProfileServiceInterface;
+use App\Application\Admin\Contracts\AdminServiceInterface;
+use App\Application\Admin\Services\AdminService;
+use App\Application\Auth\Contracts\AuthServiceInterface;
+use App\Application\Auth\Services\AuthService;
+use App\Application\Category\Contracts\CategoryServiceInterface;
+use App\Application\Category\Services\CategoryService;
+use App\Application\Expense\Contracts\ExpenseServiceInterface;
+use App\Application\Expense\Services\ExpenseService;
+use App\Application\User\Contracts\UserServiceInterface;
+use App\Application\User\Services\UserService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,10 +22,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(
-            UserServiceInterface::class,
-            UserService::class
-        );
+        $this->app->bind(UserServiceInterface::class, UserService::class);
+        $this->app->bind(AuthServiceInterface::class, AuthService::class);
+        $this->app->bind(AdminServiceInterface::class, AdminService::class);
+        $this->app->bind(AdminProfileServiceInterface::class, AdminService::class);
+        $this->app->bind(CategoryServiceInterface::class, CategoryService::class);
+        $this->app->bind(ExpenseServiceInterface::class, ExpenseService::class);
     }
 
     /**
