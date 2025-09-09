eb8873bbff7b300642fb4e4507591f9211bc7ae7
PabloSantos-CG <comercial-pablo@outlook.com>
2025-09-09 17:59:14 -0300

    refactor: remove importações e vinculações não utilizadas

--- back-end/app/Providers/AppServiceProvider.php
+++ back-end/app/Providers/AppServiceProvider.php
@@ -2,16 +2,16 @@
 
 namespace App\Providers;
 
-use App\Application\Auth\Contracts\LoginServiceInterface;
-use App\Application\Auth\Contracts\LogoutServiceInterface;
-use App\Application\Auth\Services\LoginService;
-use App\Application\Auth\Services\LogoutService;
-use App\Application\Category\Contracts\CategoryServiceInterface;
-use App\Application\Category\Services\CategoryService;
-use App\Application\Expense\Contracts\ExpenseServiceInterface;
-use App\Application\Expense\Services\ExpenseService;
-use App\Application\User\Contracts\UserServiceInterface;
-use App\Application\User\Services\UserService;
+
+// use App\Application\Category\Contracts\CategoryServiceInterface;
+// use App\Application\Category\Services\CategoryService;
+// use App\Application\Expense\Contracts\ExpenseServiceInterface;
+// use App\Application\Expense\Services\ExpenseService;
+
+use App\Services\Contracts\LoginServiceInterface;
+use App\Services\Contracts\LogoutServiceInterface;
+use App\Services\LoginService;
+use App\Services\LogoutService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,11 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(UserServiceInterface::class, UserService::class);
         $this->app->bind(LoginServiceInterface::class, LoginService::class);
         $this->app->bind(LogoutServiceInterface::class, LogoutService::class);
-        $this->app->bind(CategoryServiceInterface::class, CategoryService::class);
-        $this->app->bind(ExpenseServiceInterface::class, ExpenseService::class);
+        // $this->app->bind(CategoryServiceInterface::class, CategoryService::class);
+        // $this->app->bind(ExpenseServiceInterface::class, ExpenseService::class);
     }
 
     /**
