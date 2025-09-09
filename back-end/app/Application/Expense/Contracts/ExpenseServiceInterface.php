5c4d8e32c0aa789233b1443c8360c817b1b2dbbe
PabloSantos-CG <comercial-pablo@outlook.com>
2025-09-09 17:57:39 -0300

    refactor: remove arquivos e classes que não serão utilizados

--- back-end/app/Application/Expense/Contracts/ExpenseServiceInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Application\Expense\Contracts;
-
-interface ExpenseServiceInterface
-{
-    //
-}
--- back-end/app/Application/User/Contracts/UserServiceInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Application\User\Contracts;
-
-use App\Models\User;
-
-interface UserServiceInterface {
-    public function create(mixed $credentials): User;
-}
