2a25730c8621c39f3a3dbaa36be8a59f7def80c8
PabloSantos-CG <comercial-pablo@outlook.com>
2025-09-09 17:58:20 -0300

    refactor: herda o código de UpdateAdminProfileRequests.php

--- back-end/app/Http/Requests/UpdateUserRequest.php
+++ back-end/app/Http/Requests/UpdateUserRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\Validator;
 
 class UpdateUserRequest extends FormRequest
 {
@@ -11,7 +13,7 @@ class UpdateUserRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +24,58 @@ class UpdateUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['sometimes', 'filled', 'string', 'max:255'],
+            'email' => ['sometimes', 'filled', 'email:rfc,dns', 'unique:users'],
+            'password' => [
+                'sometimes',
+                'filled',
+                Password::min(8)
+                    ->letters()
+                    ->mixedCase()
+                    ->numbers()
+                    ->symbols(),
+            ],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'name.filled' => 'O nome não pode ser nulo.',
+            'name.string' => 'O nome deve ser uma string.',
+            'name.max' => 'O nome não pode ter mais de 255 caracteres.',
+
+            'email.filled' => 'O e-mail não pode ser nulo.',
+            'email.email' => 'E-mail inválido!',
+            'email.unique' => 'E-mail inválido!',
+
+            'password' => 'Senha inválida!'
+        ];
+    }
+
+    /**
+     * Get the "after" validation callables for the request.
+     */
+    public function after(): array
+    {
+        return [
+            function (Validator $validator) {
+                if (
+                    $this->missing('name') &&
+                    $this->missing('email') &&
+                    $this->missing('password')
+                ) {
+                    $validator->errors()->add(
+                        'empty',
+                        'É necessário enviar pelo menos um campo.'
+                    );
+                }
+            }
         ];
     }
 }
