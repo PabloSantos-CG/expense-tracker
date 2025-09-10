e938ed5b2f354546c3916729dc6e7fe59ba2b12e
PabloSantos-CG <comercial-pablo@outlook.com>
2025-09-10 17:01:33 -0300

    feat: adiciona validações para os campos permitidos na request

--- back-end/app/Http/Requests/StoreCategoryRequest.php
+++ back-end/app/Http/Requests/StoreCategoryRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class StoreCategoryRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class StoreCategoryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,26 @@ class StoreCategoryRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => ['required', 'string', 'max:255'],
+            'is_global' => [
+                Auth::user()->is_admin ? 'boolean' : 'prohibited'
+            ],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'O título não pode ser nulo.',
+            'title.string' => 'O título deve ser uma string.',
+            'title.max' => 'O título não pode ser maior do que 255 caracteres.',
+            'is_global.prohibited' => 'Unauthorized!',
+            'is_global.boolean' => 'O atributo deve ser um booleano.'
         ];
     }
 }
