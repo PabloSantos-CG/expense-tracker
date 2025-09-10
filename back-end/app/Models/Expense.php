8428f48f204cf1887bdb26a70039292ab81fe932
PabloSantos-CG <comercial-pablo@outlook.com>
2025-09-10 14:32:22 -0300

    feat: adiciona a trait para factory

--- back-end/app/Models/Expense.php
+++ back-end/app/Models/Expense.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Expense extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, HasFactory;
 
     public function user(): BelongsTo {
         return $this->belongsTo(User::class);
