2d3fb9c977bc1b4e11c4780fef678e8c583d308d
PabloSantos-CG <comercial-pablo@outlook.com>
2025-09-11 16:36:25 -0300

    feat: adiciona atributo de policy

--- back-end/app/Models/Expense.php
+++ back-end/app/Models/Expense.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use App\Policies\ExpensePolicy;
+use Illuminate\Database\Eloquent\Attributes\UsePolicy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+#[UsePolicy(ExpensePolicy::class)]
 class Expense extends Model
 {
     use SoftDeletes, HasFactory;
