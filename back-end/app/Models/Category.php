ab2323b2d7cafbcab6cd43f1ca71b3cb4754d7ca
PabloSantos-CG <comercial-pablo@outlook.com>
2025-09-11 16:04:39 -0300

    feat: adiciona atributo de policy

--- back-end/app/Models/Category.php
+++ back-end/app/Models/Category.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Policies\CategoryPolicy;
+use Illuminate\Database\Eloquent\Attributes\UsePolicy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+#[UsePolicy(CategoryPolicy::class)]
 class Category extends Model
 {
     use HasFactory;
