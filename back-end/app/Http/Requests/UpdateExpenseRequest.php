09880e340855b225479abb3cadcaf42c46f646cc
PabloSantos-CG <comercial-pablo@outlook.com>
2025-09-12 17:12:07 -0300

    feat: adiciona regras para update

--- back-end/app/Http/Requests/UpdateExpenseRequest.php
+++ back-end/app/Http/Requests/UpdateExpenseRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Validator;
 
 class UpdateExpenseRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class UpdateExpenseRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,46 @@ class UpdateExpenseRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['sometimes', 'filled', 'string', 'max:255'],
+            'value' => ['sometimes', 'filled', 'numeric', 'min:0'],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'name.filled' => 'O nome não pode ser nulo.',
+            'name.string' => 'O nome deve ser uma string.',
+            'name.max' => 'O nome não pode ter mais de 255 caracteres.',
+
+            'value.filled' => 'O valor não pode ser nulo.',
+            'value.numeric' => 'O valor deve ser um tipo numérico válido.',
+            'value.min' => 'O valor não pode ser um número negativo.',
+        ];
+    }
+
+    /**
+     * Get the "after" validation callables for the request.
+     */
+    public function after(): array
+    {
+        return [
+            function (Validator $validator) {
+                if (
+                    $this->missing('name') &&
+                    $this->missing('value')
+                ) {
+                    $validator->errors()->add(
+                        'empty',
+                        'necessário informar pelo menos um atributo.'
+                    );
+                }
+            }
         ];
     }
 }
