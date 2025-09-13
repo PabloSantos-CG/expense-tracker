5df89435af94a67b828f73ca2ae910cf5bc59ef2
PabloSantos-CG <comercial-pablo@outlook.com>
2025-09-13 10:07:52 -0300

    refactor: implementa métodos de login e logout que estavam nos services

--- back-end/app/Http/Controllers/AuthController.php
+++ back-end/app/Http/Controllers/AuthController.php
@@ -5,42 +5,35 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginAuthRequest;
 use App\Http\Requests\LogoutAuthRequest;
-use App\Services\Contracts\LoginServiceInterface;
-use App\Services\Contracts\LogoutServiceInterface;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function __construct(
-        private LoginServiceInterface $loginService,
-        private LogoutServiceInterface $logoutService,
-    ) {}
-
     public function logIn(LoginAuthRequest $request)
     {
         /** 
          * @var array{email: string, password: string} $credentials 
          */
         $credentials = $request->validated();
-        $loginSuccess = $this->loginService->execute($credentials);
 
-        if (!$loginSuccess) {
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
             return \response()->json([
-                'status' => 'error',
-                'message' => 'login failed',
-            ], 401);
+                'status' => 'success',
+                'message' => 'logged in user',
+            ]);
         }
 
-        $request->session()->regenerate();
-
         return \response()->json([
-            'status' => 'success',
-            'message' => 'logged in user',
-        ]);
+            'status' => 'error',
+            'message' => 'login failed',
+        ], 401);
     }
 
     public function logOut(LogoutAuthRequest $request)
     {
-        $this->logoutService->execute();
+        Auth::logout();
 
         $request->session()->invalidate();
         $request->session()->regenerateToken();
